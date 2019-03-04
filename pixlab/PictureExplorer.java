c5bfb639daf7925fc9e031308008576aedd0d2e6
CapitalD909 <43892704+CapitalD909@users.noreply.github.com>
2019-03-04 12:45:37 -0800

    DID SOME RANDOM STUTFF PLAY XENOBLADE

--- pixlab/PictureExplorer.java
+++ pixlab/PictureExplorer.java
@@ -800,7 +800,7 @@ public class PictureExplorer implements MouseMotionListener, ActionListener, Mou
    */
   public static void main( String args[])
   {
-    Picture pix = new Picture("beach.jpg");
+    Picture pix = new Picture("Switch_XenobladeChronicles2_illustration_14.jpg");
     pix.explore();
   }
   
