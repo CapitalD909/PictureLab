459de44e318759c2f236ae07038e485c7bc47807
CapitalD909 <43892704+CapitalD909@users.noreply.github.com>
2019-03-12 22:56:40 -0700

    FINISHED A7 BOY

--- pixlab/PictureTester.java
+++ pixlab/PictureTester.java
@@ -83,6 +83,18 @@ public class PictureTester
     temple.mirrorTemple();
     temple.explore();
   }
+  public static void testMirrorArms(){
+      Picture snowman = new Picture ("snowman.jpg");
+      snowman.explore();
+      snowman.mirrorArms();
+      snowman.explore();
+    }
+  public static void testMirrorGull(){
+      Picture gull = new Picture ("seagull.jpg");
+      gull.explore();
+      gull.mirrorGull();
+      gull.explore();
+    }
   
   /** Method to test the collage method */
   public static void testCollage()
